d890237a53c88fd65e3f4d475e8f06b2314b6e18
Djimta Kamougue <kamougued@gmail.com>
2022-05-26 12:30:51 +0200

    Start popUp
    commencement du popup

--- index.js
+++ index.js
@@ -24,26 +24,25 @@ function ChoosePhoto(nombre){
         document.getElementById("img-2").attributeStyleMap.clear();
         document.getElementById("img-3").attributeStyleMap.clear();
         document.getElementById("img-4").attributeStyleMap.clear();
-        document.getElementById("img-1").style.border='solid hsl(26, 100%, 55%) ';
-        document.getElementById("img-1").style.borderRadius='10%';
+
 
     }else if(nombre == 2 ){
         document.getElementById("item_photo_select").src="images/image-product-2.jpg";
-        document.getElementById("img-1").style.opacity= '1';
+        document.getElementById("img-1").attributeStyleMap.clear();
         document.getElementById("img-2").style.opacity= '0.33';
-        document.getElementById("img-3").style.opacity= '1';
-        document.getElementById("img-4").style.opacity= '1';
+        document.getElementById("img-3").attributeStyleMap.clear();
+        document.getElementById("img-4").attributeStyleMap.clear();;
     }else if(nombre == 3 ){
         document.getElementById("item_photo_select").src="images/image-product-3.jpg";
-        document.getElementById("img-1").style.opacity= '1';
-        document.getElementById("img-2").style.opacity= '1';
+        document.getElementById("img-1").attributeStyleMap.clear();
+        document.getElementById("img-2").attributeStyleMap.clear();
         document.getElementById("img-3").style.opacity= '0.33';
-        document.getElementById("img-4").style.opacity= '1';
+        document.getElementById("img-4").attributeStyleMap.clear();
     }else if(nombre == 4 ){
         document.getElementById("item_photo_select").src="images/image-product-4.jpg";
-        document.getElementById("img-1").style.opacity= '1';
-        document.getElementById("img-2").style.opacity= '1';
-        document.getElementById("img-3").style.opacity= '1';
+        document.getElementById("img-1").attributeStyleMap.clear();
+        document.getElementById("img-2").attributeStyleMap.clear();
+        document.getElementById("img-3").attributeStyleMap.clear();
         document.getElementById("img-4").style.opacity= '0.33';
         e.style.opacity= '0.33';
     }
