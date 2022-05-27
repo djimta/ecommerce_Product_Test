9503250df3f1c7cf81c94e3aba7f1c7cc9e45be6
djimta <kamougued@gmail.com>
2022-05-27 08:50:56 +0200

    responsible Tablette Maj

---
+++ reponsiveNavbar.js
@@ -0,0 +1,7 @@
+function openNavbar(){
+    document.querySelector('.nav_contener').style.left ='0%'
+}
+
+function closeNavbar(){
+    document.querySelector('.nav_contener').style.left= '-100%'
+}
